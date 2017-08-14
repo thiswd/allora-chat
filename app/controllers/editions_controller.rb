2045dc08b7921f5441b839ed1a53e671a300a30f
Thiago Sawada <tms1_tms2@hotmail.com>
2017-08-14 20:51:42 -0300

    Adcionei cocoon gem

--- app/controllers/editions_controller.rb
+++ app/controllers/editions_controller.rb
@@ -1,7 +1,29 @@
 class EditionsController < ApplicationController
   def new
+    @edition = Edition.new
+  end
+
+  def create
+    @edition = Edition.save(edition_params)
+    @edition.user = current_user
+    @edition.save
+
   end
 
   def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def edition_params
+    params.require(:edition).permit(:date, :greeting, :greeting_img, :farewell, :farewell_img, :user_id, posts_attributes: [:id, :headline, :option_more, :option_next, :post_img, :edition_id, :_destroy])
   end
 end
--- app/models/edition.rb
+++ app/models/edition.rb
@@ -1,4 +1,7 @@
 class Edition < ApplicationRecord
   belongs_to :user
-  has_many :posts
+  has_many :posts, inverse_of: :edition
+  accepts_nested_attributes_for :posts, reject_if: :all_blank, allow_destroy: true
+
+
 end
