50cbb762edee42ccb4c035a15b4b130f6f10e125
Thiago Sawada <tms1_tms2@hotmail.com>
2017-08-16 19:40:28 -0300

    Radio box funcionando

--- db/migrate/20170814204515_create_balloons.rb
+++ db/migrate/20170814204515_create_balloons.rb
@@ -1,7 +1,7 @@
 class CreateBalloons < ActiveRecord::Migration[5.1]
   def change
     create_table :balloons do |t|
-      t.string :content
+      t.text :content
       t.string :link
       t.string :balloon_img
       t.references :post, foreign_key: true
