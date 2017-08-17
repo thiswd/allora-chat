8dfd3838eb80761400164e168d19a0e8ec7f261b
Luana Perin <luanakperin@gmail.com>
2017-08-17 11:40:10 -0300

    alterações de navbar e link menor

--- app/models/balloon.rb
+++ app/models/balloon.rb
@@ -1,4 +1,12 @@
 class Balloon < ApplicationRecord
   belongs_to :post
   mount_uploader :balloon_img, PhotoUploader
+
+  def short_link
+    if self.link.size > 52
+      "#{self.link[0..52]}..."
+    else
+      self.link
+    end
+  end
 end
