b230ce947459e38bc0250b6cfa7bd4a1436507dc
Bruno Cerqueira <brunocerqueiram@gmail.com>
2017-08-21 16:24:24 -0300

    Permitidos position e description como params nos controllers.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170821160109) do
-
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +40,7 @@ ActiveRecord::Schema.define(version: 20170821160109) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.text "headline"
+    t.string "headline"
     t.string "option_more"
     t.string "option_next"
     t.string "post_img"
